62020065ad5517ecc073fbf6a3c6900f7e259555
Ibrahim <ibrahimdanishmand@gmail.com>
2025-05-09 15:50:56 +0500

    HELP

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : '',
   images: {
     unoptimized: true,
   },
