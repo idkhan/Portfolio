d6adfe3d411722eaec24f9bc762d3e62b4b4fec6
Ibrahim <ibrahimdanishmand@gmail.com>
2025-05-09 15:42:54 +0500

    Fixed missing images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
